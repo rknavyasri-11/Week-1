266b452241eb71f545cd3e68ea9b44f22f6921c3
rknavyasri-11 <knavya1108@gmail.com>
2026-08-27 15:57:11 +0530

    Add files via upload

---
+++ pgrm1_2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << "Enter a word: ";
+    cin >> s;
+
+    cout << "Length: " << s.length() << endl;
+
+    for (char &c : s)
+    {
+        c = toupper(c);
+    }
+
+    cout << "Upper: " << s << endl;
+
+    bool pal = true;
+    for (size_t i = 0, j = s.size() - 1; i < j; ++i, --j)
+    {
+        if (s[i] != s[j])
+        {
+            pal = false;
+            break;
+        }
+    }
+
+    if (pal)
+        cout << s << " is a palindrome" << endl;
+    else
+        cout << s << " is not a palindrome" << endl;
+
+    size_t pos = s.find("AN");
+    if (pos != string::npos)
+        cout << "AN found at index " << pos << endl;
+    else
+        cout << "AN not found" << endl;
+
+    return 0;
+}
